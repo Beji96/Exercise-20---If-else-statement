b880676ef43b3ad85304f5f7dd35b6b7f71415ce
Flavius <flaviusbejan96@yahoo.com>
2022-09-21 10:52:27 +0300

    finish

---
+++ exercise.js
@@ -0,0 +1,8 @@
+let primitive = 100;
+
+typeof primitive === "number";
+console.log("primitive is a number");
+typeof primitive === "string";
+console.log("primitive is a string");
+typeof primitive === "boolean";
+console.log("primitive is a boolean");
